e8d2709b82628bee531ab469e97d79db965b5a62
Turlx <arussellaaron@gmail.com>
2022-10-24 09:52:51 -0700

    projects

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -6,8 +6,12 @@ const [count, setCount] = useState(0)
 
 return (
 <section id='project_page'>
-    <div className='project_grid'>
+    <h1>Projects</h1>
+    <p>List of my past and current projects, sites, etc.</p>
 
+    <div className='project_type_divider' />
+
+    <div className='project_grid'>
         <div className='project_card'>
             <div>
                 <h2>Path Network</h2>
@@ -38,8 +42,56 @@ return (
             </div>
             <img src='/assets/img/projects/nginx-not-found.png' alt='project-preview' />
         </div>
+        <div className='project_card'>
+            <div>
+                <h2>as57450.net</h2>
+                <p>Autonomous system number display page, displays list of announced ip prefixes.</p>
+            </div>
+            <div className='project_card_icons'>
+                <a className='not_available_strikethrough'>
+                    <i className="fa-brands fa-github fa-xl" />
+                </a>
+                <a href='https://as57450.net'>
+                    <i className="fa-solid fa-eye fa-lg" />
+                </a>
+            </div>
+            <img src='/assets/img/projects/nginx-not-found.png' alt='project-preview' />
+        </div>
+    </div>
+    
+    <div className='project_type_divider' />
+
+    <div className='project_grid'>
+        <div className='project_card'>
+            <div>
+                <h2>DHTExporter</h2>
+                <p>Esp8266 temperature and humidity sensor using the DHT11 sensor, all data is available for prometheus to grab.</p>
+            </div>
+            <div className='project_card_icons'>
+                <a href='https://github.com/TurlxTheHuman/dhtexporter'>
+                    <i className="fa-brands fa-github fa-xl" />
+                </a>
+            </div>
+            <img src='/assets/img/projects/dhtexporter.png' alt='project-preview' />
+        </div>
+        
+        <div className='project_card'>
+            <div>
+                <h2>IPMIFan</h2>
+                <p>Fan speed control program using ipmitool, used to control fan speed for my dell poweredge r410.</p>
+            </div>
+            <div className='project_card_icons'>
+                <a href='https://github.com/TurlxTheHuman/ipmifan'>
+                    <i className="fa-brands fa-github fa-xl" />
+                </a>
+            </div>
+            <img src='/assets/img/projects/dhtexporter.png' alt='project-preview' />
+        </div>
         
     </div>
+
+    <div className='project_type_divider' />
+
 </section>
 )
 }
