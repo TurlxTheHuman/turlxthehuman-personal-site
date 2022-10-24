f11d3687f536ba95315c31071a96d5618633f0c1
Turlx <arussellaaron@gmail.com>
2022-10-23 22:46:31 -0700

    projects page

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -2,34 +2,46 @@ import { useState } from 'react'
 import '../../assets/css/global.css'
 
 function Projects() {
-  const [count, setCount] = useState(0)
+const [count, setCount] = useState(0)
 
-  return (
-    <section id='project_page'>
-      <div className='project_grid'>
+return (
+<section id='project_page'>
+    <div className='project_grid'>
+
         <div className='project_card'>
-            <h2>Project Title</h2>
-            <p>Project Description</p>
-            <img src='' alt='project-preview' />
+            <div>
+                <h2>Path Network</h2>
+                <p>A website for a network security company that specializes in ddos protection.</p>
+            </div>
+            <div className='project_card_icons'>
+                <a className='not_available_strikethrough'>
+                    <i className="fa-brands fa-github fa-xl" />
+                </a>
+                <a href='https://path.net'>
+                    <i className="fa-solid fa-eye fa-lg" />
+                </a>
+            </div>
+            <img src='/assets/img/projects/path-network.png' alt='project-preview' />
         </div>
         <div className='project_card'>
-            <h2>Project Title</h2>
-            <p>Project Description</p>
-            <img src='' alt='project-preview' />
+            <div>
+                <h2>Dropnode Hosting</h2>
+                <p>My server hosting site that offers cheap and reliable vps and game server hosting.</p>
+            </div>
+            <div className='project_card_icons'>
+                <a className='not_available_strikethrough'>
+                    <i className="fa-brands fa-github fa-xl" />
+                </a>
+                <a href='https://dropnode.net'>
+                    <i className="fa-solid fa-eye fa-lg" />
+                </a>
+            </div>
+            <img src='/assets/img/projects/nginx-not-found.png' alt='project-preview' />
         </div>
-        <div className='project_card'>
-            <h2>Project Title</h2>
-            <p>Project Description</p>
-            <img src='' alt='project-preview' />
-        </div>
-        <div className='project_card'>
-            <h2>Project Title</h2>
-            <p>Project Description</p>
-            <img src='' alt='project-preview' />
-        </div>
-      </div>
-    </section>
-  )
+        
+    </div>
+</section>
+)
 }
 
 export default Projects
