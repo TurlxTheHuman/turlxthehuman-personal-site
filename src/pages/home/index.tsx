5824d9b811a14d3e257b432c0b589dd818aa1c3f
Turlx <arussellaaron@gmail.com>
2022-10-25 10:23:07 -0700

    multiple sections
    multiple sections on index page + css scaling for projects page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,9 +5,15 @@ function Main() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='App'>
-      <section id='hero'>
-        <h1>TurlxTheHuman</h1>
+    <div className='index_page_container'>
+      <section id='hero' className='index_page_content'>
+        <h1>Name</h1>
+      </section>
+      <section id='about' className='index_page_content'>
+          <h1>About</h1>
+      </section>
+      <section id='contact' className='index_page_content'>
+          <h1>Contact</h1>
       </section>
     </div>
   )
